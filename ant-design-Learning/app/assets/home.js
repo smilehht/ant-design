485e102dc7e154d5742cf67c7ea941d896fb72b2
huhongtao <956184070@qq.com>
2017-03-21 00:25:33 +0800

    HomePage

---
+++ ant-design-Learning/app/assets/home.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Layout , Menu, Breadcrumb, Icon} from 'antd';
+import './home.css';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const { SubMenu } = Menu;
+
+const header_footer={   
+    backgroundColor: '#7DBCEA'
+};
+
+const sider={
+    backgroundColor: '#3BA0E9'
+}
+
+export default class Home extends React.Component{
+    render(){
+        return <Layout>
+            <Header style={ header_footer }>
+                {/*<div className='logo'/>*/}
+                <Menu
+                    theme='dark'
+                    mode='horizontal'
+                    defaultSeletedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key='1'>Menu 3</Menu.Item>
+                    <Menu.Item key='2'>Menu 1</Menu.Item>
+                    <Menu.Item key='3'>Menu 3</Menu.Item>
+                    <Menu.Item key='4'>Menu 4</Menu.Item>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider width={200} style={{ background: '#fff' }}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{ height: '100%' }}
+                    >
+                        <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                            <Menu.Item key="1">option1</Menu.Item>
+                            <Menu.Item key="2">option2</Menu.Item>
+                            <Menu.Item key="3">option3</Menu.Item>
+                            <Menu.Item key="4">option4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                            <Menu.Item key="5">option5</Menu.Item>
+                            <Menu.Item key="6">option6</Menu.Item>
+                            <Menu.Item key="7">option7</Menu.Item>
+                            <Menu.Item key="8">option8</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                            <Menu.Item key="9">option9</Menu.Item>
+                            <Menu.Item key="10">option10</Menu.Item>
+                            <Menu.Item key="11">option11</Menu.Item>
+                            <Menu.Item key="12">option12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Breadcrumb style={{ margin: '12px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>;
+    }
+}
